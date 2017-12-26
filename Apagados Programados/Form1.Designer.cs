5c79f4390732db0c9677d2452406a371c6b71670
PowerOfSombrero <34815607+PowerOfSombrero@users.noreply.github.com>
2017-12-26 17:06:51 +0100

    Efficiency changes

---
+++ Apagados Programados/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Apagados_Programados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelH = new System.Windows.Forms.Label();
+            this.horas = new System.Windows.Forms.TextBox();
+            this.minutos = new System.Windows.Forms.TextBox();
+            this.segundos = new System.Windows.Forms.TextBox();
+            this.Apagado = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Reinicio = new System.Windows.Forms.Button();
+            this.Cancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelH
+            // 
+            this.labelH.AutoSize = true;
+            this.labelH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelH.ForeColor = System.Drawing.Color.Snow;
+            this.labelH.Location = new System.Drawing.Point(163, 23);
+            this.labelH.Name = "labelH";
+            this.labelH.Size = new System.Drawing.Size(391, 20);
+            this.labelH.TabIndex = 0;
+            this.labelH.Tag = "labelH";
+            this.labelH.Text = "Indica el tiempo para el Apagado/Reinicio programado\r\n";
+            this.labelH.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // horas
+            // 
+            this.horas.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.horas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.horas.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.horas.ForeColor = System.Drawing.SystemColors.Window;
+            this.horas.Location = new System.Drawing.Point(213, 98);
+            this.horas.Name = "horas";
+            this.horas.Size = new System.Drawing.Size(63, 44);
+            this.horas.TabIndex = 3;
+            this.horas.Text = "00";
+            // 
+            // minutos
+            // 
+            this.minutos.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.minutos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.minutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutos.ForeColor = System.Drawing.SystemColors.Window;
+            this.minutos.Location = new System.Drawing.Point(314, 98);
+            this.minutos.Name = "minutos";
+            this.minutos.Size = new System.Drawing.Size(63, 44);
+            this.minutos.TabIndex = 4;
+            this.minutos.Text = "00";
+            // 
+            // segundos
+            // 
+            this.segundos.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.segundos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.segundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.segundos.ForeColor = System.Drawing.SystemColors.Window;
+            this.segundos.Location = new System.Drawing.Point(413, 98);
+            this.segundos.Name = "segundos";
+            this.segundos.Size = new System.Drawing.Size(63, 44);
+            this.segundos.TabIndex = 5;
+            this.segundos.Text = "00";
+            // 
+            // Apagado
+            // 
+            this.Apagado.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Apagado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Apagado.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Apagado.FlatAppearance.BorderSize = 0;
+            this.Apagado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Apagado.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Apagado.Location = new System.Drawing.Point(29, 196);
+            this.Apagado.Name = "Apagado";
+            this.Apagado.Size = new System.Drawing.Size(96, 36);
+            this.Apagado.TabIndex = 6;
+            this.Apagado.Tag = "Apagado";
+            this.Apagado.Text = "Apagado";
+            this.Apagado.UseVisualStyleBackColor = false;
+            this.Apagado.Click += new System.EventHandler(this.Apagado_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(281, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(380, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ":";
+            // 
+            // Reinicio
+            // 
+            this.Reinicio.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Reinicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Reinicio.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Reinicio.FlatAppearance.BorderSize = 0;
+            this.Reinicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reinicio.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Reinicio.Location = new System.Drawing.Point(156, 196);
+            this.Reinicio.Name = "Reinicio";
+            this.Reinicio.Size = new System.Drawing.Size(96, 36);
+            this.Reinicio.TabIndex = 9;
+            this.Reinicio.Tag = "Reinicio";
+            this.Reinicio.Text = "Reinicio";
+            this.Reinicio.UseVisualStyleBackColor = false;
+            this.Reinicio.Click += new System.EventHandler(this.Reinicio_Click);
+            // 
+            // Cancelar
+            // 
+            this.Cancelar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Cancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Cancelar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Cancelar.FlatAppearance.BorderSize = 0;
+            this.Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancelar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Cancelar.Location = new System.Drawing.Point(557, 196);
+            this.Cancelar.Name = "Cancelar";
+            this.Cancelar.Size = new System.Drawing.Size(96, 36);
+            this.Cancelar.TabIndex = 10;
+            this.Cancelar.Tag = "Cancelar";
+            this.Cancelar.Text = "Cancelar";
+            this.Cancelar.UseVisualStyleBackColor = false;
+            this.Cancelar.Click += new System.EventHandler(this.Cancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(684, 255);
+            this.Controls.Add(this.Cancelar);
+            this.Controls.Add(this.Reinicio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Apagado);
+            this.Controls.Add(this.segundos);
+            this.Controls.Add(this.minutos);
+            this.Controls.Add(this.horas);
+            this.Controls.Add(this.labelH);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelH;
+        private System.Windows.Forms.TextBox horas;
+        private System.Windows.Forms.TextBox minutos;
+        private System.Windows.Forms.TextBox segundos;
+        private System.Windows.Forms.Button Apagado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Reinicio;
+        private System.Windows.Forms.Button Cancelar;
+    }
+}
+
---
+++ Apagados Programados/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apagados_Programados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Apagado_Click(object sender, EventArgs e)
+        {
+            //Apagado programado del Sistema
+            int total = PasarASegundos();
+
+            //LANZAMOS EL COMANDO PARA QUE LA CMD LO EJECUTE
+            String apagado = "shutdown -s -t " + total;
+            System.Diagnostics.ProcessStartInfo processStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + apagado);
+
+            //Indicamos que no se muestre la pantalla de cmd
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = false;
+
+            //Inicializamos el proceso
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = processStartInfo;
+            proc.Start();
+
+        }
+
+        private void Cancelar_Click(object sender, EventArgs e)
+        {
+            //Cancela tanto reinicios como apagados
+            String cancelar = "shutdown /a";
+
+            //Creamos proceso para lanzar el comando a la cmd del sistema
+            System.Diagnostics.ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c" + cancelar);
+
+            //Indicamos que no se muestre la pantalla de cmd
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = false;
+
+            //Inicializamos el proceso
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = processStartInfo;
+            proc.Start();
+
+
+        }
+
+        private void Reinicio_Click(object sender, EventArgs e)
+        {
+                //Reinicios programados del sistema
+
+                int total = PasarASegundos();
+
+                //LANZAMOS EL COMANDO PARA QUE LA CMD LO EJECUTE
+                String reinicio = "shutdown -r -t " + total;
+                System.Diagnostics.ProcessStartInfo processStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + reinicio);
+
+                //Indicamos que no se muestre la pantalla de cmd
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = false;
+
+                //Inicializamos el proceso
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = processStartInfo;
+                proc.Start();           
+
+        }
+
+        public int PasarASegundos()
+        {
+
+            String h = horas.Text;
+            String m = minutos.Text;
+            String s = segundos.Text;
+
+            try
+            {
+                int numHoras = Int32.Parse(h);
+                int numMin = Int32.Parse(m);
+                int numSegun = Int32.Parse(s);
+
+                int hTotal = numHoras * 3600;
+                int mTotal = numMin * 60;
+
+                int total = hTotal + mTotal + numSegun;
+
+                //MessageBox.Show(total.ToString());
+
+                return total;
+
+            }
+            catch (System.FormatException) //Control de errores
+            {
+                MessageBox.Show("ERROR: Debes escribir números, no letras");
+
+                return 0;
+            }
+      
+        }
+    }
+}
